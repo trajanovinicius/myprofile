cbdc87605cdc0ddb0f281db44c0408ee79c97a39
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2023-06-14 17:11:35 -0300

    refatorando

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Profile from "../src/pages/Profile";
-import TechSection from "./components/techSection";
 import { GlobalStyle } from "./styles/global";
 function App() {
   return (
     <>
       <GlobalStyle />
       <Profile />
-      <TechSection/>
     </>
   );
 }
