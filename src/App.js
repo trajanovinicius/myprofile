01a020e221bf5f517ce0594cb7d5ed9da7d684bc
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2023-06-14 17:08:37 -0300

    iniciando desenvolvimento da seção de tecnologias

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Profile from "../src/pages/Profile";
+import TechSection from "./components/techSection";
 import { GlobalStyle } from "./styles/global";
 function App() {
   return (
     <>
       <GlobalStyle />
       <Profile />
+      <TechSection/>
     </>
   );
 }
