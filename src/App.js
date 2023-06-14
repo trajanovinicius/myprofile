d35bf91c47e2aaa13838877da68a6f4fb9942c4d
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2023-06-14 18:10:49 -0300

    reafatorando

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import { Route } from "react-router-dom";
 import Profile from "../src/pages/Profile";
 import { GlobalStyle } from "./styles/global";
-import Technologies from "./pages/Technologies";
 function App() {
   return (
     <>
       <GlobalStyle />
       <Profile />
-      <Route path="/technologies" element={<Technologies />} />
     </>
   );
 }
